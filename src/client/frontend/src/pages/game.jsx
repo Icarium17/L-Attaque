6b5496680c562f20b1afa56471e8cfe6821af846
Eddy HUART <e.ehuart@etu.cvm.qc.ca>
2026-03-18 15:33:46 -0400

    Ajout background game.jsx

--- src/client/frontend/src/pages/game.jsx
+++ src/client/frontend/src/pages/game.jsx
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import MainLayout from "../layouts/main-layout";
+import Button from "../components/button.jsx";
+import Notification from "../components/notification.jsx";
 
-export default function Game(){
+import backgroundGame from '../assets/images/background-game.png';
+ 
 
-    return (<h1>hi</h1>)
+export default function Game() {
+  const [session, setSession] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const key = localStorage.getItem("sessionKey");
+    const username = localStorage.getItem("username");
+    
+    if (!key || !username) {
+      navigate("/");
+    } else {
+      setSession({ username, key });
+    }
+  }, [navigate]);
+
+  return (
+    <MainLayout
+      title="Game - L'Attaque"
+      background={backgroundGame}
+      session={session}
+      hideMenu={true}
+    >
+      <div>hi</div>
+    </MainLayout>
+  );
 }
--- src/client/frontend/src/pages/lobby.jsx
+++ src/client/frontend/src/pages/lobby.jsx
@@ -12,13 +12,12 @@ export default function Lobby() {
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
-  // 1. Vérification de la session au montage
+  // Si pas en session redirige vers index.jsx
   useEffect(() => {
     const key = localStorage.getItem("sessionKey");
     const username = localStorage.getItem("username");
     
     if (!key || !username) {
-      //navigate("/"); // Retour à l'accueil si non connecté
     } else {
       setSession({ username, key });
     }
@@ -36,103 +35,71 @@ export default function Lobby() {
       session={session}
       hideMenu={true}
     >
-   
-      <div className="relative flex flex-col items-center justify-center">
-        
- 
+      <div className="relative flex flex-col items-center justify-center">        
         <div className="absolute inset-0 bg-gray-950/50" />
- 
-        <div className="relative z-10 flex flex-col items-center justify-center w-full px-6">
-          
+         <div className="relative z-10 flex flex-col items-center justify-center w-full px-6">          
           <img
             src={logo}
             alt="Logo du Jeu"
-            className="max-w-full md:max-w-2xl h-auto drop-shadow-2xl mb-12"
+            className="max-w-full md:max-w-3xl h-auto drop-shadow-2xl mb-12"
           />
         </div>
-         </div>
-      
-         <div className="absolute top-[21.5%] left-[4%] w-[22%] flex flex-col gap-[3%] z-10">
+         </div>      
+         <div className="absolute top-[21.5%] left-[3.5%] w-[22%] flex flex-col gap-[3%] z-10">
                 <Button 
                 variant="ghost"
                 onClick={goToGame} 
-                className="w-full sm:w-2/3 text-[42px]"
+                className="w-full sm:w-2/3 text-[clamp(16px,2.1vw,32px)]"
                 >
                 NOUVEAU JEU
                 </Button>  
-
           </div>
-
-            <div className="absolute top-[34%] left-[4%] w-[22%] flex flex-col gap-[3%] z-10">
+            <div className="absolute top-[34%] left-[3.5%] w-[22%] flex flex-col gap-[3%] z-10">
                 <Button 
                 variant="ghost"
                 onClick={goToGame} 
-                className="w-full sm:w-2/3 text-[42px]"
+                className="w-full sm:w-2/3 text-[clamp(16px,2.1vw,32px)]"
                 >
                 REPRENDRE
                 </Button>  
-
           </div>
-
-            <div className="absolute top-[46%] left-[4%] w-[22%] flex flex-col gap-[3%] z-10">
+            <div className="absolute top-[46%] left-[3.5%] w-[22%] flex flex-col gap-[3%] z-10">
                 <Button 
                 variant="ghost"
                 onClick={goToGame} 
-                className="w-full sm:w-2/3 text-[42px]"
+                className="w-full sm:w-2/3 text-[clamp(16px,2.1vw,32px)]"
                 >
                 CLASSEMENT
                 </Button>  
-
           </div>
-
-       <div className="absolute top-[59%] left-[4%] w-[22%] flex flex-col gap-[3%] z-10">
+       <div className="absolute top-[59%] left-[3.5%] w-[22%] flex flex-col gap-[3%] z-10">
                 <Button 
                 variant="ghost"
                 onClick={goToGame} 
-                className="w-full sm:w-2/3 text-[42px]"
+                className="w-full sm:w-2/3 text-[clamp(16px,2.1vw,32px)]"
                 >
                 OPTIONS
                 </Button>  
-
           </div>
-
-      <div className="absolute top-[71%] left-[4%] w-[22%] flex flex-col gap-[3%] z-10">
+      <div className="absolute top-[71%] left-[3.5%] w-[22%] flex flex-col gap-[3%] z-10">
                 <Button 
                 variant="ghost"
                 onClick={goToGame} 
-                className="w-full sm:w-2/3 text-[42px]"
+                className="w-full sm:w-2/3 text-[clamp(16px,2.1vw,32px)]"
                 >
                 QUITTER
-                </Button>  
-
-                   
-          </div>
-          
-                <div className="absolute top-[90%] left-[45.7%] w-[22%] flex flex-col gap-[3%] z-10">
+                </Button>                     
+          </div>          
+                <div className="absolute top-[90%] left-[46%] w-[22%] flex flex-col gap-[3%] z-10">
                 <Button 
                 variant="ghost"
                 onClick={goToGame} 
-                className="w-full sm:w-2/3 text-[37px]"
+                className="w-full sm:w-2/3 text-[clamp(16px,1.6vw,28px)]"
                 >
                 TUTORIEL
-                </Button>  
-
-                   
+                </Button>                     
           </div>
-          
-
-          
-
-          
-
-          
-
-
- 
           {error && <div className="mt-4"><Notification message={error} /></div>}
-          
-       
-     
-    </MainLayout>
+      </MainLayout>
   );
 }
