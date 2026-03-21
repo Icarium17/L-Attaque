a8b59ece69f8c48991f8e0675c33554aff51447e
Charlee Dupras <146775518+WhatyouEgg17@users.noreply.github.com>
2026-03-21 13:51:46 -0400

    BeliefPiece class implementation
    Added BeliefPiece class to manage piece probabilities and updates.

--- src/serveur/Working/GAME/piece.py
+++ src/serveur/Working/GAME/piece.py
@@ -34,3 +34,31 @@ class Piece():
         self.type = type
         self.position = position
         self.owner = owner ## int avec l'ordre du joueur
+
+class BeliefPiece(Piece):
+    def __init__(self, id, position, owner):
+        super.__init__(self, id, None, position, owner)
+        self.probabilities = self.set_probabilities() # TODO : À modifier quand le jeu va fonctionner, pour que tout puisse être updaté rapidement.
+        
+    def set_probabilities(self):
+        total_pieces = sum(piece.count for piece in PieceType)
+        for piece in PieceType:
+            self.probabilities[piece] = piece.count / total_pieces
+
+    def normalize(self): //when a piece becomes impossible (when it moves, it cant be a flag...)
+        s = sum(self.probabilities.values())
+        if s == 0:
+            return ("No more ennemy pieces")
+        for type in self.probabilities:
+            prob.probabilities(type) /= s
+
+    def update_probabilities(self, pieces_left):
+        total_pieces = sum(remaining_count.values())
+
+        for type in self.probabilities:
+            if self.probabilities[type] > 0 :
+                self.probabilities[type] = pieces_left[type] / total_pieces
+            else:
+                self.probabilities[type] = 0
+
+        self.normalize()
