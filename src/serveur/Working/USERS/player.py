12d3ec5e0fc54ea3544d11940bac1e6452cd49d9
Charlee Dupras <146775518+WhatyouEgg17@users.noreply.github.com>
2026-03-21 14:17:00 -0400

    Enhance Player class with piece management

--- src/serveur/Working/USERS/player.py
+++ src/serveur/Working/USERS/player.py
@@ -1,5 +1,6 @@
 from USERS.user import User
 from GAME.board import Board
+from GAME.piece import PieceType
 
 class Player(User):
     def __init__(self, user_instance, order, time_remaining = (30*60)):
@@ -7,21 +8,26 @@ class Player(User):
         self.order = order
         self.known_board = None
         self.time_remaining = time_remaining
+        self.pieces_left = self.position_pieces()
+        self.belief_pieces = None
 
+    def initialize_piece_left(self):
+        for piece in PieceType:
+            self.pieces_left[piece] = piece.count
+    
     def move(self, move):
         self.known_board.move(move)
 
     def position_pieces(self, pieces):
         self.known_board.set_pieces(pieces)
 
-    def initialize_game(self):
-        pass
+    def update belief_states(self, piece_to_remove):
+        piece_to_remove_type = piece_to_remove.type
+        self.pieces_left[piece_to_remove_type] -= 1
+        for belief_piece in self.belief_pieces:
+            belief_piece.update_probabilities(self.pieces_left)
 
-    def turn(self):
-        pass
-
-    def update_belief_state(self):
-        pass
+    
 
 
     
