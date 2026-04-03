f8d3251329f8d1836e7e0ac25d90f326ef37c5fc
Eddy HUART <e.ehuart@etu.cvm.qc.ca>
2026-04-03 10:06:09 -0400

    Ajout drag drop dans cell.jsx

--- src/client/frontend/src/components/cell.jsx
+++ src/client/frontend/src/components/cell.jsx
@@ -6,21 +6,31 @@ import cellSelected from '../assets/images/cell-selected.png';
 import cellValid from '../assets/images/cell-valid.png';
 import Piece  from "../components/piece.jsx";
  
-export default function Cell({ row, col, isLake, piece, onClick, isSelected, isValidMove, playerColor }) {
-  let texture;
-  if (isLake) {
-    texture = cellLake;
-  } else if (isSelected) {
-    texture = cellSelected;
-  } else if (isValidMove) {
-    texture = cellValid;
-  } else {
-    texture = cellTexture;
-  }
+export default function Cell({ row, col, isLake, piece, onClick, isSelected, isValidMove, playerColor, onDragStart, onDrop }) {
+let texture;
+if (isLake) {
+  texture = cellLake;
+} else if (isSelected) {
+  texture = cellSelected;
+} else if (isValidMove) {
+  texture = cellValid;
+} else {
+  texture = cellTexture;
+}
+
+const isDraggable = onDragStart != null && piece != null;
 
  return (
     <div
       onClick={() => onClick(row, col)}
+      draggable={isDraggable}
+      onDragStart={isDraggable ? (e) => {
+        e.dataTransfer.effectAllowed = "move";
+        onDragStart({ type: "board", row, col });
+      } : undefined}
+      onDragOver={onDrop ? (e) => { e.preventDefault(); } : undefined}
+      onDrop={onDrop ? (e) => { e.preventDefault(); onDrop(row, col); } : undefined}
+
       className={`
         aspect-square flex items-center justify-center
         ${isLake ? "cursor-not-allowed" : "cursor-pointer hover:brightness-125"}
