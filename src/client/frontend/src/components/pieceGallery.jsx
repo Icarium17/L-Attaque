4af125ec8030a9fee881c9a0218de4d814d0a521
e.ehuart@etu.cvm.qc.ca <J@cksonJ@ckson79>
2026-05-28 07:56:01 -0400

    Mise a jour tutorial fhd

--- src/client/frontend/src/components/pieceGallery.jsx
+++ src/client/frontend/src/components/pieceGallery.jsx
@@ -17,21 +17,21 @@ const PIECES_INFO = [
 
 export default function PieceGallery({ playerColor = "BLUE" }) {
   return (
-    <div className="min-h-full w-full flex items-center justify-center py-12">
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-22 gap-y-4 justify-items-center w-full max-w-6xl">
+    <div className="min-h-full w-full flex items-center justify-center py-4 fhd:py-6 4k:py-10">
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-1 fhd:gap-x-10 fhd:gap-y-2 4k:gap-x-16 4k:gap-y-4 justify-items-center w-full max-w-6xl fhd:max-w-7xl 4k:max-w-[1600px]">
         {PIECES_INFO.map((p) => (
           <div key={p.type} className="flex flex-col items-center text-center group">
-            <div className="scale-80 transition-transform">
+            <div className="scale-65 fhd:scale-75 4k:scale-100 transition-transform">
               <PieceCard piece={{ rank: p.rank, type: p.type, player: playerColor }} />
             </div>
-            
-            <h3 className="text-yellow-300 font-bold text-lg uppercase tracking-wider">
+
+            <h3 className="text-yellow-300 font-bold text-base fhd:text-lg 4k:text-2xl uppercase tracking-wider">
               {p.type}
             </h3>
-            <p className="text-white text-md font-semibold">
+            <p className="text-white text-sm fhd:text-md 4k:text-xl font-semibold">
               Quantité : <span className="text-yellow-500">{p.count}</span>
             </p>
-            <p className="text-gray-200 text-sm leading-tight max-w-45 italic">
+            <p className="text-gray-200 text-xs fhd:text-sm 4k:text-lg leading-tight max-w-40 fhd:max-w-45 4k:max-w-64 italic">
               {p.desc}
             </p>
           </div>
